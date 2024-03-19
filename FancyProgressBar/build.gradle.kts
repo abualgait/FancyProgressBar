e442091a791f64085e4cfc05c45d04944e8b094c
MuhammadSayed <mohamed.sayed@salesrabbit.com>
2024-03-19 14:58:25 +0200

    docs: add jitpack configuration files and settings

--- FancyProgressBar/build.gradle.kts
+++ FancyProgressBar/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -38,7 +39,15 @@ android {
     }
 
 }
-
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            afterEvaluate {
+                from(components["release"])
+            }
+        }
+    }
+}
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
